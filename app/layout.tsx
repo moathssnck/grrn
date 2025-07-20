82615f0517386b42a078050b2a81496b02ab8dd3
moathssnck <myneww00@gmail.com>
2025-07-20 20:22:22 +0300

    sss

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'جميع المنتجات  ',
+  description: ' تقديم وتطوير خدمات توريد وتقديم الذبائح والمواشي بشكل متطور لمواكبة الحياة العصرية.',
 }
 
 export default function RootLayout({
